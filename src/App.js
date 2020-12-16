aed1cf701cf873459ed6b699c62a40ef2173626d
Heather Luttrell <possumdiva@heatherluttrell.com>
2020-12-15 22:06:27 -0500

    added redux, radio buttons and all 49 questions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./style.css";
+import questionAPI from './question/questions';
+import QuestionBox from './components/QuestionBox';
+import Result from './components/ResultBox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      questionBank: [],
+
+    };
+  }
+
+  setAnswers = (id, answer) => {
+    const questionBank = this.state.questionBank.map(question => {
+      if (question.id === id) {
+        return { ...question, response: answer }
+      }
+      return question
+    })
+    this.setState({ questionBank })
+  }
+
+
+
+
+  // Function to get question from ./question 
+  getQuestions = () => {
+    questionAPI().then(question => {
+      console.log(question);
+      this.setState({ questionBank: question });
+    });
+  };
+
+  // Set state back to default and call function
+  // playAgain = () => {
+  //   this.getQuestions();
+  //   this.setState({ score: 0, responses: 0 });
+  // };
+
+  // // Function to compute scores
+  // computeAnswer = (answer, correctAns) => {
+  //   if (answer === correctAns) {
+  //     this.setState({
+  //       score: this.state.score + 1
+  //     });
+  //   }
+  //   this.setState({
+  //     responses: this.state.responses < 5
+  //       ? this.state.responses + 1
+  //       : 5
+  //   });
+  // };
+
+  // componentDidMount function to get question 
+  componentDidMount() {
+    this.getQuestions();
+  }
+
+  render() {
+    return (<div className="container">
+      <div className="glow">ChakraHarmony</div>
+
+      {this.state.questionBank.length > 0 &&
+        // this.state.responses < 5 &&
+        this.state.questionBank.map((question, index) => <QuestionBox question=
+          {question.question} options={question.answers} key={question.questionId + index}
+          Id={question.questionId} />)}
+
+
+
+      {
+        this.state.responses === 49
+          ? (<Result score={this.state.score}
+            playAgain={this.playAgain} />)
+          : null
+      }
+
+    </div>)
+  }
 }
+
 
 export default App;
---
+++ src/question/questions.js
@@ -0,0 +1,399 @@
+const qBank = [
+    {
+        question:
+            "Are you experiencing any feelings of restlessness or insecurity?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "01",
+        type: "Root"
+    },
+    {
+        question:
+            "Are your legs feeling weak?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "02",
+        type: "Root"
+    },
+    {
+        question:
+            "Are you having issues with your Bladder or Kidneys?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "03",
+        type: "Root"
+    },
+    {
+        question:
+            "Are you experiencing pain or weakness in your lower back?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "04",
+        type: "Root"
+    },
+    {
+        question:
+            "Is constipation a recent or ongoing issue?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "05",
+        type: "Root"
+    },
+    {
+        question:
+            "Are you having feelings of anger or aggression?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "06",
+        type: "Root"
+    },
+    {
+        question:
+            "Are you experiencing any strong feelings towards material things or experiencing sensations of greediness?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "07",
+        type: "Root"
+    },
+    {
+        question:
+            "Are you experiencing reproductive issues or any changes in your sex drive?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "08",
+        type: "Sacral"
+    },
+    {
+        question:
+            "Do you feel like you cannot commit to a relationship?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "09",
+        type: "Sacral"
+    },
+    {
+        question:
+            "Do you have feelings of impotence or feel like you are being secretly betrayed?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "10",
+        type: "Sacral"
+    },
+    {
+        question:
+            "Are you having trouble expressing your emotions?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "11",
+        type: "Sacral"
+    },
+    {
+        question:
+            "Are you having trouble with your kidneys or your urinary system",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "12",
+        type: "Sacral"
+    },
+    {
+        question:
+            " Have you been experiencing pelvic issues?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "13",
+        type: "Sacral"
+    },
+    {
+        question:
+            "Are you having issues with your creativity or problems experiencing pleasure in your everyday life?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "14",
+        type: "Sacral"
+    },
+    {
+        question:
+            "Have you had issues with digestion or stomach ulcers?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "15",
+        type: "SolarPlexus"
+    },
+    {
+        question:
+            " Have you had any liver problems, whether with alcohol or not?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "16",
+        type: "SolarPlexus"
+    },
+    {
+        question:
+            " Do you experience chronic fatigue or issues with your energy levels?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "17",
+        type: "SolarPlexus"
+    },
+    {
+        question:
+            " Do you feel strong in your feelings of personal power/self-esteem?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "18",
+        type: "SolarPlexus"
+    },
+    {
+        question:
+            "Are you feeling extremely self-critical?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "19",
+        type: "SolarPlexus"
+    },
+    {
+        question:
+            "Are you feeling extremely sensitive to criticism?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "20",
+        type: "SolarPlexus"
+    },
+    {
+        question:
+            " Have you been overly concerned with your physical appearance?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "21",
+        type: "SolarPlexus"
+    },
+    {
+        question:
+            "Do you have troubles with asthma or lung disease?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "22",
+        type: "Heart"
+    },
+    {
+        question:
+            "Have you been experiencing pain in your upper back or shoulders?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "23",
+        type: "Heart"
+    },
+    {
+        question:
+            "Have other expressed that they feel smothered by your attentions or affections?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "24",
+        type: "Heart"
+    },
+    {
+        question:
+            "Are feelings of jealousy coming up a lot lately, whether it be in love, or work, or just jealousy of others in general??",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "25",
+        type: "Heart"
+    },
+    {
+        question:
+            "Have you been experiencing a lot of anger lately, or fear of abandonment?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "26",
+        type: "Heart"
+    },
+    {
+        question:
+            "Are you having trouble with your arms or wrists?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "27",
+        type: "Heart"
+    },
+    {
+        question:
+            "Are you scared to be single/alone?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "28",
+        type: "Heart"
+    },
+    {
+        question:
+            "Have you been having issues with your thyroid?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "29",
+        type: "Throat"
+    },
+    {
+        question:
+            "Do you feel like you have been communicating well with others?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "30",
+        type: "Throat"
+    },
+    {
+        question:
+            "Are you having any pain in your neck or cervical spine issues?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "31",
+        type: "Throat"
+    },
+    {
+        question:
+            "Do you feel like you have been having trouble with your willpower?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "32",
+        type: "Throat"
+    },
+    {
+        question:
+            "Are you scared of losing your personal power or feel limited in your personal choices?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "33",
+        type: "Throat"
+    },
+    {
+        question:
+            "Are you feeling strong in your ability to express yourself?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "34",
+        type: "Throat"
+    },
+    {
+        question:
+            "Have you had any recent bouts of laryngitis or a sore throat?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "35",
+        type: "Throat"
+    },
+    {
+        question:
+            "AHave you had any issues with blurred vision or eyestrain?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "36",
+        type: "ThirdEye"
+    },
+    {
+        question:
+            "Have you been having frequent headaches?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "37",
+        type: "ThirdEye"
+    },
+    {
+        question:
+            "Do you find yourself frequently drifting off or daydreaming lately?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "38",
+        type: "ThirdEye"
+    },
+    {
+        question:
+            "Are you experiencing frequent mood swings?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "39",
+        type: "ThirdEye"
+    },
+    {
+        question:
+            " Are you having trouble with your sinuses or nasal passages?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "40",
+        type: "ThirdEye"
+    },
+    {
+        question:
+            "Do you feel you have been having trouble facing your fears?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "41",
+        type: "ThirdEye"
+    },
+    {
+        question:
+            "Are you quick to anger/volitility?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "42",
+        type: "ThirdEye"
+    },
+    {
+        question:
+            "Have you been experiencing issues with depression or a sense of loss?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "43",
+        type: "Crown"
+    },
+    {
+        question:
+            "Are you having trouble focusing or trouble learning new concepts?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "44",
+        type: "Crown"
+    },
+    {
+        question:
+            "Are you very set in your thinking on religion or spirituality?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "45",
+        type: "Crown"
+    },
+    {
+        question:
+            "Are you frequently experiencing feelings of confusion?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "46",
+        type: "Crown"
+    },
+    {
+        question:
+            "Have you been unusually sensitive to light, sound and your general environment?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "47",
+        type: "Crown"
+    },
+    {
+        question:
+            "Are you feeling or scared you will begin to feel alienated from your friends or family?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "48",
+        type: "Crown"
+    },
+    {
+        question:
+            "Are you having issues with your sense of self-knowing or greater power?",
+        answers: ["yes", "no"],
+        correct: "yes",
+        questionId: "49",
+        type: "Crown"
+    },
+
+];
+
+// n = 5 to export 5 question
+export default (n = 49) =>
+    Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
